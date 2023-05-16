9c1577526fcefa64aa34e36bc39d505ff2124857
Vladimir Lalabekyan <davidvovchik@gmail.com>
2023-05-16 15:52:42 +0300

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters) {
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        key += characters.charAt(Math.floor(Math.random() * characters.length));
+    };
+    return key;
+};
+const key = generateKey(16, characters);
+console.log(key);
